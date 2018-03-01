8adcb50edcc97e9b93993bef188bb8f521b120fd
andrecasal <andrecasal@protonmail.com>
2018-03-01 18:01:53 +0000

    Initial commit

---
+++ spec/ftp-upload-on-change-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FtpUploadOnChangeView from '../lib/ftp-upload-on-change-view';
+
+describe('FtpUploadOnChangeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
