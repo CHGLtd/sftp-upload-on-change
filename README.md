3ff33d3f199d2ee5020354878c4583b24295f376
andrecasal <andrecasal@protonmail.com>
2018-03-02 17:06:31 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ftp-upload-on-change
 
-FTP upload files on change package for Atom
+FTP upload files on change package for Atom (In development)
+
+Currently watches for files to upload, but doesn't upload yet.
--- spec/ftp-upload-on-change-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FtpUploadOnChangeView from '../lib/ftp-upload-on-change-view';
-
-describe('FtpUploadOnChangeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
